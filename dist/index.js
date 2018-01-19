3feb92f2ef9dd1285e5f0a6523cc7b040b90d75b
igor poughtach <igor@igors-MBP.corp.moonsite.co.il>
2018-01-19 12:28:50 +0200

    initial

---
+++ dist/index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+/**
+ * 
+ */
+
+var _config = {
+    designWidth: 0,
+    deviceWidth: 0,
+    ratio: 1,
+    roundToNearestPoint: false,
+    middlewear: null,
+}
+
+function _roundToNearestPoint(points){
+    let rounded = Math.floor(points)
+    let fraction = points - rounded
+
+    if(fraction > 0.49) return rounded + 1
+    else return rounded
+}
+
+exports.configure = function({
+        designWidth = _config.designWidth,
+        deviceWidth = _config.deviceWidth,
+        roundToNearestPoint = _config.roundToNearestPoint,
+        middlewear = _config.middlewear
+} = {}){
+    let ratio = designWidth / deviceWidth
+
+    _config = {
+        ..._config,
+        designWidth,
+        deviceWidth,
+        roundToNearestPoint,
+        ratio,
+        middlewear
+    }
+}
+
+exports.calculate = function(pixels = 0, {
+    roundToNearestPoint = _config.roundToNearestPoint,
+    middlewear = _config.middlewear,
+    designWidth = _config.designWidth,
+    deviceWidth = _config.deviceWidth
+} = {}){
+    let { ratio } = _config
+    if(!designWidth) throw new Error('Design width is not set')
+    if(!deviceWidth) throw new Error('Dvice width is not set')
+    if(!pixels) return 0
+    
+    let dp = pixels / ratio
+
+    if(roundToNearestPoint)
+        dp = _roundToNearestPoint(dp)
+
+    if(middlewear)
+        dp = middlewear(dp)
+
+    return dp
+}
---
+++ example.js
@@ -0,0 +1,38 @@
+const pixels2points = require('./src')
+
+const calcSize = pixels2points.calculate
+
+// configure device and design width in pixels
+pixels2points.configure({
+    deviceWidth: 640,
+    designWidth: 750
+})
+
+console.log(calcSize(200)) // 170.66666666666666
+
+// use built in roundeing function to round output to nearest integer
+pixels2points.configure({
+    roundToNearestPoint: true
+})
+
+console.log('rounded', calcSize(200)) // 171
+
+const myRoundingFunction = function(points){
+    return Math.floor(points)
+}
+
+// configure pixels2points to use custom middlewear function
+pixels2points.configure({
+    roundToNearestPoint: false,
+    middlewear: myRoundingFunction
+})
+
+console.log('rounded down', calcSize(200)) // 170
+
+// run with parameters
+var buttonWidth = calcSize(200, {
+    deviceWidth: 720,
+    middlewear: points => points > 150 ? 120 : 100
+})
+
+console.log('responsive button width = ', buttonWidth) // 120
